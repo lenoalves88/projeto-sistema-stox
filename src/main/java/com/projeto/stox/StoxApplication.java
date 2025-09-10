a97c58016e243f4c27dc77c009f4b47ad3acaddd
leno alves <ruyleno88@gmail.com>
2025-09-10 12:44:39 -0300

    primeiro commit

---
+++ src/main/java/com/projeto/stox/StoxApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.stox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/stox/StoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.stox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
